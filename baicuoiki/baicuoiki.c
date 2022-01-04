ac750e051792e5d74df3ef2ec44d0dd0f9198ae1
hoangSnapeV <90324439+hoangSnapeV@users.noreply.github.com>
2022-01-04 08:12:26 +0700

    oke

--- baicuoiki/baicuoiki.c
+++ baicuoiki/baicuoiki.c
@@ -40,7 +40,7 @@ typedef struct {
     char chu[100];
 } call;
 
-int demSodong(const char file_path[])
+int demSoDong(const char file_path[])
 {
     FILE* file = fopen(file_path, "r");
 
@@ -57,6 +57,24 @@ int demSodong(const char file_path[])
     return count;
 }
 
+int count_dssm(const char file_path[])
+{
+    FILE* file = fopen(file_path, "r");
+
+    int count = 0;
+    char line[1000];
+    fgets(line, 1000, file);
+    while (!feof(file))
+    {   
+        
+        fgets(line, 1000, file);
+        count++;
+    }
+    fclose(file);
+    return count;
+}
+
+//
 dssv* read_file(const char file_path[], int n)
 {
     FILE* file = fopen(file_path, "r");
@@ -156,6 +174,24 @@ date* bring_date(dssv* list_student, int n)
     return date_sts;
 }
 
+void create_error(const char path[])
+{
+    FILE* file = fopen(path, "w");
+    fprintf(file, "%s", "invalid command");
+
+    fclose(file);
+}
+
+void create_emptyFile(const char path[])
+{
+    FILE* file = fopen(path, "w");
+    
+    fprintf(file, "%s", ' ');
+
+    fclose(file);
+}
+
+
 //y1
 void print_sts_list(dssv* my_sts, int n, char x[], const char path[])
 {   
@@ -167,12 +203,16 @@ void print_sts_list(dssv* my_sts, int n, char x[], const char path[])
 
         if(strcmp(my_sts[i].classname, x) == 0)
         {   
-            fprintf(file,"%d -  %d %s %s %s %s %s %s\n", index, my_sts[i].studentID, my_sts[i].firstName, my_sts[i].lastName, my_sts[i].gender, my_sts[i].dateOfBirth, my_sts[i].classname, my_sts[i].country);
+            fprintf(file,"%d %s %s %s %s %s %s\n", my_sts[i].studentID, my_sts[i].firstName, my_sts[i].lastName, my_sts[i].gender, my_sts[i].dateOfBirth, my_sts[i].classname, my_sts[i].country);
             index++;
-        }
+        } 
 
     }
-
+    if (index == 0)
+    {
+        create_emptyFile("result.csv");            
+    }
+    
     fclose(file);
     
 }
@@ -210,7 +250,7 @@ void count_Female(dssv* my_sts, int n, const char path[])
     fclose(file);
 }
 
-//y4 sort asc là tang dân
+//y4 
 void sort_asc(dssv* my_sts, int n, const char path[], date* listdate)
 {
     FILE* file = fopen(path, "w");
@@ -280,14 +320,14 @@ void sort_asc(dssv* my_sts, int n, const char path[], date* listdate)
     {   
         //print_student(my_sts[i], x);
 
-        fprintf(file,"%d -  %d %s %s %s %s %s %s\n", i+1, my_sts[i].studentID, my_sts[i].firstName, my_sts[i].lastName, my_sts[i].gender, my_sts[i].dateOfBirth, my_sts[i].classname, my_sts[i].country);
+        fprintf(file,"%d %s %s %s %s %s %s\n", my_sts[i].studentID, my_sts[i].firstName, my_sts[i].lastName, my_sts[i].gender, my_sts[i].dateOfBirth, my_sts[i].classname, my_sts[i].country);
         
 
     }
 
     fclose(file);
 }
-//giam sort desc
+
 void sort_desc(dssv* my_sts, int n, const char path[], date* listdate)
 {
     FILE* file = fopen(path, "w");
@@ -356,7 +396,7 @@ void sort_desc(dssv* my_sts, int n, const char path[], date* listdate)
     {   
         //print_student(my_sts[i], x);
 
-        fprintf(file,"%d -  %d %s %s %s %s %s %s\n", i + 1, my_sts[i].studentID, my_sts[i].firstName, my_sts[i].lastName, my_sts[i].gender, my_sts[i].dateOfBirth, my_sts[i].classname, my_sts[i].country);
+        fprintf(file,"%d %s %s %s %s %s %s\n", my_sts[i].studentID, my_sts[i].firstName, my_sts[i].lastName, my_sts[i].gender, my_sts[i].dateOfBirth, my_sts[i].classname, my_sts[i].country);
         
 
     }
@@ -384,17 +424,16 @@ void print_country(dssv* my_sts, int n, char x[], const char path[])
     
 }
 
-////////
 /// y3
-diem* read_file_diem(const char file_path[], int n)
+diem* read_file_diem(const char file_path[], int n, int a)
 {
     FILE* file = fopen(file_path, "r");
-    diem * diemlist = calloc((n - 1) * 6, sizeof(diem));
+    diem * diemlist = calloc((n - 1) * a, sizeof(diem));
 
     char line[200];
     fgets(line, 200, file);
 
-    for (int i = 0; i <= (n - 1) * 6 - 1; i++)
+    for (int i = 0; i <= (n - 1) * a - 1; i++)
     {
         fgets(line, 200, file);
 
@@ -510,30 +549,12 @@ void print_top(list_avg* list_grade_avg, int n, const char path[], dssv* list_st
     }
     for (int i = 0; i <= x - 1; i++)
     {   
-        fprintf(file,"%d - %d %s %s %s %s %s %s\n",i,list_student[i].studentID, list_student[i].firstName, list_student[i].lastName, list_student[i].gender, list_student[i].dateOfBirth, list_student[i].classname, list_student[i].country);
+        fprintf(file,"%d %s %s %s %s %s %s\n", list_student[i].studentID, list_student[i].firstName, list_student[i].lastName, list_student[i].gender, list_student[i].dateOfBirth, list_student[i].classname, list_student[i].country);
     }
     fclose(file);
 }
 
-// check---
-
-void create_error(const char path[])
-{
-    FILE* file = fopen(path, "w");
-    fprintf(file, "%s", "invalid command");
-
-    fclose(file);
-}
-
-void create_emptyFile(const char path[])
-{
-    FILE* file = fopen(path, "w");
-    
-    fprintf(file, "%s", ' ');
-
-    fclose(file);
-}
-
+//--
 int check_chuoi(char name[])
 {   
     int n = strlen(name);
@@ -557,6 +578,11 @@ int check_chuoi(char name[])
                 }
             }
     }
+    if (count_space == 0)
+    {
+        return 0;
+    }
+    
     
     return 1;
 }
@@ -570,33 +596,34 @@ void chuanHoa_lenh(char name[])
     }
 }
 
-void chuanHoa_Chu(char thamso[], char lenh[])
+int chuanHoa_Chu_1(char thamso[])
+{
+    int n = strlen(thamso);
+    
+    thamso[0] = toupper(thamso[0]);
+    for (int i = 1; i < n; i++)
+    {
+        thamso[i] = tolower(thamso[i]);
+    }
+    
+
+}
+
+void chuanHoa_Chu_sort(char thamso[])
 {
     int n = strlen(thamso);
 
-    if(strcmp(lenh, "count") == 0 || strcmp(lenh, "country") == 0)
-    {   
-        thamso[0] = toupper(thamso[0]);
-        for (int i = 1; i < n; i++)
-        {
-            thamso[i] = tolower(thamso[i]);
-        }
-    }
-
-    if(strcmp(lenh, "sort") == 0)
+    
+    for (int i = 0; i < n; i++)
     {
-        for (int i = 0; i < n; i++)
-        {
-            thamso[i] = tolower(thamso[i]);
-        }
-    }   
+        thamso[i] = tolower(thamso[i]);
+    }
 
 }
 
 int chuanHoa_So(char thamso[])
 {
     int n = strlen(thamso);
-    
     for (int i = 0; i < n; i++)
     {
         int check = isdigit(thamso[i]);
@@ -610,23 +637,47 @@ int chuanHoa_So(char thamso[])
 
     return a;
 }
+//
+int check_thamso_chu(char thamso[])
+{
+    int n = strlen(thamso);
+    
+    for (int i = 0; i < n; i++)
+    {
+        int check = isalpha(thamso[i]);
+        if(check == 0) return 0;  
+        
+    }
+    return 1;
+}
 
+void print_txt( const char file_log[], int n, int s, const char file_lenh[], char x[])
+{
+    
+    FILE* file_1 = fopen(file_log, "w");
+    FILE* file_2 = fopen(file_lenh, "w");
+
+    fprintf(file_1, "%d %d %d", n - 1, s, (n - 1) * s);
+    fprintf(file_2, "%s", x);
+
+    fclose(file_1);
+    fclose(file_2);
+}
 
 int main()
 {   
     
-    int n = demSodong("dssv.csv");
+    int n = demSoDong("dssv.csv");
+    int s = count_dssm("dsmh.csv");
 
     dssv* list_student = read_file("dssv.csv", n);
     dssv* copy_list = read_file("dssv.csv", n);
 
     date* list_date = bring_date(copy_list, n);  
 
-    diem* list_diem_sts = read_file_diem("diem.csv", n);
+    diem* list_diem_sts = read_file_diem("diem.csv", n, s);
 
     list_avg* list_grade_avg = grade_average(list_diem_sts, n);
-    
-    //printf("%d", n);
 
     ///
     
@@ -634,10 +685,13 @@ int main()
     printf("nhap lenh: ");
     fgets(caulenh, 100, stdin);
     caulenh[strlen(caulenh) - 1] = '\0';
+    
+    print_txt("log.txt", n, s, "cau lenh.txt", caulenh);
 
-    //printf("%lu\n", strlen(caulenh));
+    
     int result = check_chuoi(caulenh);
     call nhap_lenh;
+
     if (result)
     {       
         char* token = strtok(caulenh, " ");
@@ -655,32 +709,95 @@ int main()
             token = strtok(NULL, " ");
             index++;
         }
-        
-    }
 
-    chuanHoa_lenh(nhap_lenh.lenh);
-    chuanHoa_Chu(nhap_lenh.chu, nhap_lenh.lenh);
-    int kq = chuanHoa_So(nhap_lenh.chu);
-    
+        chuanHoa_lenh(nhap_lenh.lenh);
+
+        int a1 = strcmp(nhap_lenh.lenh, "list") == 0;
+        int a2 = strcmp(nhap_lenh.lenh, "count") == 0;
+        int a3 = strcmp(nhap_lenh.lenh, "country") == 0;
+        int a4 = strcmp(nhap_lenh.lenh, "top") == 0;
+        int a5 = strcmp(nhap_lenh.lenh, "sort") == 0;
+
+        if (a1 || a2 || a3 || a4 || a5)
+        {            
+            //print_sts_class
+            if(strcmp(nhap_lenh.lenh, "list") == 0)
+            {
+                print_sts_list(list_student, n, nhap_lenh.chu, "result.csv");
+            }
+
+            int result_1 = check_thamso_chu(nhap_lenh.chu);
+            //count
+            if(strcmp(nhap_lenh.lenh, "count") == 0)
+            {
+                if(result_1)
+                {
+                    chuanHoa_Chu_1(nhap_lenh.chu); 
+                    
+                    if (strcmp(nhap_lenh.chu, "Male") == 0)
+                    {
+                        count_male(list_student, n,  "result.csv"); 
+                    } else if(strcmp(nhap_lenh.chu, "Female") == 0)
+                        {
+                            count_Female(list_student, n,  "result.csv");
+                        } else
+                            {
+                                create_error("error.txt");
+                            }   
+                    
+                }
+            }
+            //country
+            if(strcmp(nhap_lenh.lenh, "country") == 0)
+            {
+                if(result_1)
+                {
+                    chuanHoa_Chu_1(nhap_lenh.chu);
+
+                    print_country(list_student, n, nhap_lenh.chu, "result.csv");
+                }
+            }
+            // top n
+            if (strcmp(nhap_lenh.lenh, "top") == 0)
+            {
+                int result2 = chuanHoa_So(nhap_lenh.chu);
+                
+                if (result2 > 0)
+                {
+                    print_top(list_grade_avg, n, "result.csv", list_student, result2);
+                } else
+                    {
+                        create_error("error.txt");
+                    }
+            }
+
+            //sort
+            if (strcmp(nhap_lenh.lenh, "sort") == 0)
+            {   
+                chuanHoa_Chu_sort(nhap_lenh.chu);
+                if (strcmp(nhap_lenh.chu, "asc") == 0)
+                {
+                    sort_asc(list_student, n,  "result.csv", list_date);
+                } else if (strcmp(nhap_lenh.chu, "desc") == 0)
+                    {
+                        sort_desc(list_student, n,  "result.csv", list_date);
+                    } else
+                        {
+                            create_error("error.txt");
+                        }
+            }
+        } else
+            {
+                create_error("error.txt");
+            } 
+
+
+    } else 
+        {
+            create_error("error.txt");
+        }
+
     printf("%s %s\n", nhap_lenh.lenh, nhap_lenh.chu );
-    //create_error("error.txt");
-    ////    
-
-    print_sts_list(list_student, n, nhap_lenh.chu, "result.csv");
-
-    //count_male(list_student, n,  "result.csv");
-
-    //count_Female(list_student, n,  "result.csv");
-
-    //sort_asc(list_student, n,  "result.csv", list_date);
-    //sort_desc(list_student, n,  "result.csv", list_date);
-
-    //print_country(list_student, n, "China", "result.csv");
-
-    //y3
-       
-
-    //print_top(list_grade_avg, n, "result.csv", list_student, 123456);
 
     return 0;
 }
